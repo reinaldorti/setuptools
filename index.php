1c313b762d09b755dbcbf8d0d6d6e72b5b924d58
Reinaldo Dorti <reinaldorti@gmail.com>
2020-08-29 10:50:18 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$array = [
+    'name' => 'Reinaldo',
+    'lastname' => 'Dorti',
+    'email' => 'reinaldorti@gmail.com',
+    'telephone' => '17 999-9999',
+    'number' => 2
+];
+
+var_dump($array);
